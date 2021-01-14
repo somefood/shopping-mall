c195bd93ae7715b30103d83382b6fd3264e948e8
Seokju Hong <iamseokjuhong@gmail.com>
2021-01-14 12:04:24 +0900

    카테고리 url 끝에 id 있을 시, category 객체 반환

--- products/views.py
+++ products/views.py
@@ -11,10 +11,12 @@ class ProductListView(ListView):
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['categories'] = get_list_or_404(Category)
+        context['selected_category'] = self.selected_category
         return context
 
     def get(self, request, *args, **kwargs):
         if 'pk' in kwargs:
+            self.selected_category = Category.objects.get(pk=kwargs['pk'])
             self.queryset = Product.objects.filter(category=kwargs['pk'])
         return super().get(request, *args, **kwargs)
 
